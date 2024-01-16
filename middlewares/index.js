0087573e1bc8ab711b255208b8295b73febf54bb
Cajamarquina <martorella.ana@gmail.com>
2024-01-16 11:27:32 +0100

    update

---
+++ middlewares/index.js
@@ -0,0 +1,4 @@
+export { default as isEmptyBody } from "./isEmptyBody.js";
+export { default as isValidId } from "./isValidId.js";
+export { default as userAuthenticate } from "./userAuthenticate.js";
+export { default as upload } from "./upload.js";
